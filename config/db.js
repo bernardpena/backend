bc738dd7a185dfd730a1b3af644b666b88c8cab9
Bernardo <bernardpena@gmail.com>
2024-12-01 13:50:12 -0300

    railway 2

--- config/db.js
+++ config/db.js
@@ -8,19 +8,4 @@ const pool = new Pool({
   port: 5432,
 });
 
-const { Client } = require("pg");
-
-// Asegúrate de que DATABASE_PRIVATE_URL esté configurada en el entorno de Railway
-const client = new Client({
-  connectionString:
-    process.env.DATABASE_PRIVATE_URL || process.env.DATABASE_PUBLIC_URL,
-});
-
-client
-  .connect()
-  .then(() => console.log("Conectado a la base de datos"))
-  .catch((err) => {
-    console.error("Error al conectar a la base de datos:", err);
-  });
-
 module.exports = pool;
--- index.js
+++ index.js
@@ -41,3 +41,18 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
+
+const { Client } = require("pg");
+
+// Asegúrate de que DATABASE_PRIVATE_URL esté configurada en el entorno de Railway
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_PRIVATE_URL || process.env.DATABASE_PUBLIC_URL,
+});
+
+client
+  .connect()
+  .then(() => console.log("Conectado a la base de datos"))
+  .catch((err) => {
+    console.error("Error al conectar a la base de datos:", err);
+  });
